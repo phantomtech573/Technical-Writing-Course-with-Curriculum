8d53a15b4827b260bccfbe6f2d46d0cf00eb45e2
MGlenn <phantomtech573@gmail.com>
2026-03-09 13:37:29 -0500

    Update config.c

--- config.c
+++ config.c
@@ -2,7 +2,7 @@ version: 0.1.1
 achievements: Modification by Pull Request ultimately a Merged Commit
   ~$:.achievement: Established Access
       name: Hackmamba Star
-      icon: be what I want to be https://raw.githubusercontent.com/hackmamba-io/Technical-Writing-Curriculum/main/.vaunt/hackmamba_star.png
+      icon: "https://raw.githubusercontent.com/phantomtech573/vaunt/Flozura_star.png"
       description: Awarded for starring our repository, make a wish!
       triggers: run, go, config, install, clean, echo, nonmatch
         ~$:.trigger:any
